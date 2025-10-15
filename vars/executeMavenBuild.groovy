44896d89c764d8b466fa5a48a7490ef853ab7e99
SG-Val <sundaravignesh.gnanasekaran@valeo.com>
2025-10-15 15:20:14 +0530

    Update executeMavenBuild.groovy updated Maven details for robustness

--- vars/executeMavenBuild.groovy
+++ vars/executeMavenBuild.groovy
@@ -1,4 +1,7 @@
 def call() {
+    // This step finds the location of the Maven tool and returns its path
+    def mvnHome = tool 'Maven-3.9.11'
+    
     // This is the logic from your old 'Build' stage
     script {
         currentBuild.description = "Commit: ${env.GIT_COMMIT}"
